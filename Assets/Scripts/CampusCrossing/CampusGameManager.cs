e8da1ae007b355e9feb764a45573e58eea6f1de3
EddyTian58 <bowentian58@gmail.com>
2025-03-31 04:39:28 -0700

    implemented basic UI and timer

--- Assets/Scripts/CampusCrossing/CampusGameManager.cs
+++ Assets/Scripts/CampusCrossing/CampusGameManager.cs
@@ -1,15 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CampusGameManager : MonoBehaviour
 {
     [Header("Goal Settings")]
-    [Tooltip("goal tag")]
     public string goalTag = "ccGoal";
-
-    [Tooltip("Current goal")]
     [SerializeField] private GameObject currentActiveGoal;
     private GameObject CurrentActiveGoal => currentActiveGoal;
     private Goal goal;
@@ -24,11 +23,33 @@ public class CampusGameManager : MonoBehaviour
     public bool hideInactiveGoals = true;
     public bool disableInactiveInteractions = true;
 
+    [Header("Timer Settings")]
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float initialTimerDuration = 60f;
+    [SerializeField] private bool timerActive = false;
+    [SerializeField] private string timerFormat = "0.0";
+    private float currentTime;
+
+    // event to fire when timer reaches zero
+    public UnityEvent onTimerFinished = new UnityEvent();
+
+    [Header("Player Input Control")]
+    [SerializeField] private float inputDisableDuration = 3f;
+    [SerializeField] private TextMeshProUGUI startText;
+    private bool playerInputDisabled = false;
+    private float inputDisableTimer = 0f;
+
+    [Header("Other")]
+    [SerializeField] private TextMeshProUGUI goalText;
+
     [Header("Debug")]
     public bool showDebug = true;
 
     private List<GameObject> allGoals = new List<GameObject>();
     [SerializeField] private int goalsHit = 0;
+    [SerializeField] private string goalName;
+    public string GoalName => goalName;
+
     private void Awake()
     {
         FindAllGoalsInScene();
@@ -49,6 +70,88 @@ public class CampusGameManager : MonoBehaviour
 
         InitializeAllGoals();
         SelectRandomGoal();
+        InitializeTimer();
+        DisablePlayerInput();
+        ResetGame();
+    }
+
+    private void InitializeTimer()
+    {
+        // set starting timer value
+        currentTime = initialTimerDuration;
+        UpdateTimerDisplay();
+
+        if (timerText == null && showDebug)
+        {
+            Debug.LogWarning("Timer TextMeshPro component is not assigned!");
+        }
+    }
+
+    public void StartTimer()
+    {
+        timerActive = true;
+        if (showDebug) Debug.Log("Timer started");
+    }
+
+    public void StopTimer()
+    {
+        timerActive = false;
+        if (showDebug) Debug.Log("Timer stopped");
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = initialTimerDuration;
+        UpdateTimerDisplay();
+        if (showDebug) Debug.Log("Timer reset");
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        if (timerText != null)
+        {
+            timerText.text = "Time before class starts: " + currentTime.ToString(timerFormat);
+        }
+    }
+
+    public void DisablePlayerInput()
+    {
+        playerInputDisabled = true;
+        inputDisableTimer = inputDisableDuration;
+
+        // show start texting text
+        if (startText != null)
+        {
+            startText.gameObject.SetActive(true);
+        }
+
+        StopTimer();
+
+        if (showDebug) Debug.Log($"Player input disabled for {inputDisableDuration} seconds");
+    }
+
+    public void EnablePlayerInput()
+    {
+        playerInputDisabled = false;
+
+        // hide starting text
+        if (startText != null)
+        {
+            startText.gameObject.SetActive(false);
+        }
+
+        // unhide timer and goal text, start timer
+        timerText.enabled = true;
+        goalText.enabled = true;
+        StartTimer();
+
+        if (showDebug) Debug.Log("Player input enabled");
+    }
+
+    // public function to access player input on/off variable
+    public bool isInputAllowed()
+    {
+        return !playerInputDisabled;
     }
 
     private void FindAllGoalsInScene()
@@ -97,9 +200,17 @@ public class CampusGameManager : MonoBehaviour
 
         currentActiveGoal = availableGoals[Random.Range(0, availableGoals.Count)];
         SetGoalState(currentActiveGoal, true);
-
+        UpdateGoalName(currentActiveGoal.name);
+        goalText.text = "Get to the " + goalName + " building!";
 
         if (showDebug) Debug.Log($"New goal selected: {currentActiveGoal.name}", currentActiveGoal);
+    }
+
+    private void UpdateGoalName(string newGoalName)
+    {
+        goalName = newGoalName.ToLower();
+
+        if (showDebug) Debug.Log($"Goal name updated to: {goalName}");
     }
 
     public void OnGoalReached()
@@ -133,8 +244,51 @@ public class CampusGameManager : MonoBehaviour
             SelectRandomGoal();
         }
     }
+
+    private void ResetGame()
+    {
+        timerText.enabled = false;
+        goalText.enabled = false;
+    }
+
     void Update()
     {
+        // update input disabling timer
+        if (playerInputDisabled)
+        {
+            inputDisableTimer -= Time.deltaTime;
+
+            // update start text
+            if (startText != null)
+            {
+                startText.text = "Your next class is in the " + goalName + " building! \n Don't be late!";
+            }
+
+            if (inputDisableTimer <= 0)
+            {
+                EnablePlayerInput();
+            }
+        }
+
+        // tick timer
+        if (timerActive)
+        {
+            currentTime -= Time.deltaTime;
+
+            // check if timer has hit zero
+            if (currentTime <= 0f)
+            {
+                currentTime = 0f;
+                timerActive = false;
+
+                onTimerFinished.Invoke();
+
+                if (showDebug) Debug.Log("Timer finished!");
+            }
+
+            UpdateTimerDisplay();
+        }
+
         if (Input.GetKeyUp(KeyCode.Escape) && showDebug == true)
         {
             SelectRandomGoal();
